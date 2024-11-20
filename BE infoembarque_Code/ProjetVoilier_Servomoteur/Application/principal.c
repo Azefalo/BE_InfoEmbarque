2a4ad1f44e0771912925253edf79013567c033ac
peterthemercenary <pierrediamanesenghor@esp.sn>
2024-11-20 16:14:38 +0100

    last version of our codes

--- BE infoembarque_Code/ProjetVoilier_Servomoteur/Application/principal.c
+++ BE infoembarque_Code/ProjetVoilier_Servomoteur/Application/principal.c
@@ -1,28 +1,14 @@
+#include "stm32f10x.h"
 #include "MyGPIO.h"
 #include "MyTimer.h"
-#include "stm32f10x.h"
-#include "MyEncoder.h"
+#include "MyADC.h"
 
-
-
-
-int main(void) {
-	  //initialisation les pins
-	  Encoder_GPIO_Init();
-    // Initialisation du Timer 2 en mode encodeur
-    Encoder_Timer_Init();
-	  // Déclarer une broche avec Z pour reset
-	  MyGPIO_Struct_TypeDef index;
-	  // Initialisation de la LED sur PA5 (output push-pull)
-    index.GPIO = GPIOA;
-    index.GPIO_Pin = 4;
-    index.GPIO_Conf = In_Floating;
-    MyGPIO_Init(&index);
-   	int z= MyGPIO_Read(index.GPIO,index.GPIO_Pin);
-	
-		int Teta=0;
+ 
+int main(void)
+{ 
+		int Teta=120;
 		int alpha=0;
-		MyGPIO_Struct_TypeDef Consigne_S;//On met notre consigne du servo moteur sur B0 vu quon utilise TIM3
+		MyGPIO_Struct_TypeDef Consigne_S;//On met notre consigne du servo moteur sur B0 vu qu'on utilise TIM3
     Consigne_S.GPIO = GPIOB;
     Consigne_S.GPIO_Pin = 0;
     Consigne_S.GPIO_Conf = AltOut_Ppull;
@@ -34,33 +20,18 @@ int main(void) {
 	  tim_SV.PSC=719;
 	  MyTimer_Base_Init(&tim_SV);
 		
-		MyTimer_PWM(tim_SV.Timer,3);//On met en marche la PWM
+		MyTimer_PWM(tim_SV.Timer,3);//On met
 		MyTimer_Base_Start(TIM3);
-		int encoder_position=0;
-			
-    while (1) {
-			  //Lire le signal de référence de l'encodeur
-			  z= MyGPIO_Read(index.GPIO,index.GPIO_Pin);
-        // Lire la position de l'encodeur
-        encoder_position = MyTimer_Encoder_Read(TIM2);
-			  if(z==1){
-					MyTimer_Encoder_Reset(); //EXTI et/ou AFIO a faire apres
-		    }
-				
-				alpha= (encoder_position/4);
-				
-				if (alpha >180){
-					alpha=360-alpha;
-				}
-				
-				if (alpha >=0 & alpha<=45){
-					Teta=180;
-				}
-				if (alpha >=45 & alpha<=180){
-					Teta=240-180*alpha/135;
-				}
+
+	  while(1){
 		
-				MyTimer_SetDutyCycle(tim_SV.Timer ,3 ,(Teta/36)+5);
-			
-    }
+		if (alpha >=0 & alpha<=45){
+			Teta=180;
+		}
+		if (alpha >=45 & alpha<=180){
+			Teta=240-180*alpha/(180-45);
+		}
+		
+		MyTimer_SetDutyCycle(tim_SV.Timer ,3 ,(Teta/24)+5);
+	}
 }
