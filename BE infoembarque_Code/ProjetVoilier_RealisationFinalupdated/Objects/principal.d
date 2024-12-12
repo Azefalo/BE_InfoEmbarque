8ca8c61b5a82c1a9d031d734575a2057f1044747
peterthemercenary <pierrediamanesenghor@esp.sn>
2024-12-12 16:15:31 +0100

    Send data update zith angle roulis

---
+++ BE infoembarque_Code/ProjetVoilier_RealisationFinalupdated/Objects/principal.d
@@ -0,0 +1,8 @@
+./objects/principal.o: Application\principal.c \
+  C:\Users\logar\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
+  RTE\_Reel\RTE_Components.h \
+  C:\Users\logar\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Users\logar\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h \
+  ..\Driver\MyI2C.h ..\Driver\DS1307.h ..\Driver\MyUSART.h \
+  ..\Driver\MyGPIO.h ..\Driver\MyADC.h ..\Driver\MyTimer.h \
+  ..\Driver\MyPLATEAU.h ..\Driver\MyEncoder.h ..\Driver\MySPI.h
