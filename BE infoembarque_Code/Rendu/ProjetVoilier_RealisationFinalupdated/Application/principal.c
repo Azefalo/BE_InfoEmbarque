b565ef8ea755b19b6518ff8b0ca527fa3d787830
MarcinsaS <msanche1@insa-toulouse.fr>
2024-12-23 20:43:28 +0100

    principal.c commentaire

--- BE infoembarque_Code/Rendu/ProjetVoilier_RealisationFinalupdated/Application/principal.c
+++ BE infoembarque_Code/Rendu/ProjetVoilier_RealisationFinalupdated/Application/principal.c
@@ -1,24 +1,23 @@
 #include "myApplication.h"
 
 int main(void){
-		// Initialisation de l'horloge du système (RCC) et des GPIO nécessaires
-    //SystemInit();
-	  initPlateau();
-	  UART1_Init();
-    Battery_init();
+
+	  initPlateau(); //initialisation de la commande du plateau  
+	  UART1_Init(); // communication USART activé en mode réception et transmission 
+    Battery_init();  //initialisation de la lecture de la batterie
 	  DS1307_TimeDate MARC;
-    RTC_init(&MARC);
-	  Roulis_init();
+    RTC_init(&MARC); //initialisattion de la lecture du RTC
+	  Roulis_init(); //initialisation pour déterminer l'angle de roulis
 		
 		float angle_roulis;
 		
-		//initialisation les pins
+		//initialisation des pins de l'encodeur (A et B)
 	  Encoder_GPIO_Init();
-    // Initialisation du Timer 2 en mode encodeur
+    // Initialisation du Timer 2 en mode encodeur 3 pour lire A et B
     Encoder_Timer_Init();
-	  // Déclarer une broche avec Z pour reset
+	  // définition de la pin permettant de lire z pour reset alpha
 	  MyGPIO_Struct_TypeDef index;
-    Index_init(&index); //initialisation de l'index Z de l'encodeur
+    Index_init(&index); //initialisation de l'index z de l'encodeur
 	
 	  MyTimer_Struct_TypeDef tim_SV;
     Servo_init(&tim_SV); //initialisation du servomoteur
@@ -28,13 +27,13 @@ int main(void){
 				angle_roulis = Demande_message();
 				
 				if (angle_roulis>=90){
-					angle_roulis = fabs(angle_roulis-180);
+					angle_roulis = fabs(angle_roulis-180); // mise à jour de l'angle de roulis 
 				}
 				
-        Index_test(&index);
+        Index_test(&index); //réinitialisation de l'angle alpha de la girouette dès que l'index z est atteint 
 
-        Servo_update(&tim_SV, angle_roulis);
+        Servo_update(&tim_SV, angle_roulis); // mise à jour de l'angle du servomoteur en fonction de la position  de la girouette
 		
-				Send_Data(&MARC, angle_roulis);
+				Send_Data(&MARC, angle_roulis); //envoi des données utiles à la télécommande
 		 }
 }
